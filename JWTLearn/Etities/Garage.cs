095882c262f18e2f3b9ce53e82cf0176baad0b7d
Souruss <soupirot.dev@gmail.com>
2024-06-21 16:43:00 +0200

    modifiaction de la bdd

--- JWTLearn/Etities/Garage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Etities;
-
-public class Garage : EntityBase
-{
-    public string Name { get; set; }
-    public string Address { get; set; }
-    public List<Voiture> Voitures { get; set; } = [];
-}
